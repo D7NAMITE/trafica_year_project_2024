c132572ad8e4253bda402d00c11b3cf5e5440107
Phumrapee Chaowanapricha <phumrapeechaowanapricha@Phumrapees-MacBook-Pro-2.local>
2024-05-10 15:05:10 +0700

    Create test_get_max_aqi_us_success for successful response of /api/aqi/max/aqi_us endpoint.

--- test_api.py
+++ test_api.py
@@ -41,6 +41,14 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.json())
     
+    def test_get_max_aqi_us_success(self):
+        """
+        Test case for successful response of /api/aqi/max/aqi_us endpoint.
+        """
+        response = self.client.get("/api/aqi/max/aqi_us")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.json())
+    
     def test_get_daily_avg_noise_success(self):
         """
         Test case for successful response of /api/noise/avg/daily endpoint.
